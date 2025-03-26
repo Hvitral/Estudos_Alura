da561d36896fbec86f07efaa048d3507a9db3ced
Henrique Vitral Silva Andrade <hvitral@hotmail.com>
2025-03-26 13:28:42 -0300

    Curso_de_Lógica_de_Programação
    Contém todos os arquivos utilizados no decorrer do curso

---
+++ Curso de Lógica de programação/Desafios/desafio-js-projeto_inicial/logica-js-desafio/app.js
@@ -0,0 +1,83 @@
+alert('Bem vindo ao desafio');
+
+let numeroUMDEZ = 0;
+
+while(numeroUMDEZ != 2){
+    numeroUMDEZ ++
+    console.log(numeroUMDEZ)
+};
+
+console.log('----------------------------------------------------------') ;
+
+let numeroDEZUM = 2;
+
+while(numeroDEZUM != 0){
+    numeroDEZUM -- 
+    console.log(numeroDEZUM)
+};
+
+console.log('Contagem regressiva');
+
+let contagemRegressiva = prompt('Digite um número para a contagem regressiva:');
+
+while(contagemRegressiva != 0){
+    contagemRegressiva -- 
+    console.log(contagemRegressiva)
+};
+
+console.log('Contagem progressiva');
+
+let numeroMaximo = prompt("Digite um número para a contagem progregressiva:");;
+let contador = 0;
+
+while (contador <= numeroMaximo) {
+    console.log(contador);
+    contador++
+}
+
+
+/*let zero = 0;
+let contagemProgressiva = prompt('Digite um número para a contagem progressiva:')
+
+while(zero <= contagemProgressiva){
+    zero ++
+    console.log(contagemProgressiva)
+}*/
+
+
+
+
+
+
+
+
+/*let diaDaSemana = prompt ('Qual o dia da semana:');
+
+if (diaDaSemana == ("Sabado", "Domingo")) {
+    alert('Bom Fim de Semana!')
+} else {
+    alert('Boa Semana!')
+};
+
+let numDigitado = prompt ('Digite um número de -100 até 100:');
+if (numDigitado >= 0){
+    alert('o número digitado é positivo')
+} else {
+    alert('O número digitado é negativo')
+}
+
+pontuacao = 105;
+if (pontuacao >= 100) {
+    console.log('Parabéns, você venceu!');
+} else {
+    console.log('Tente novamente para ganhar.');
+}
+
+let saldoConta = 500; // Exemplo de saldo
+alert(`Seu saldo é de R$${saldoConta}.`);
+
+let nome = prompt('Qual o seu nome?');
+alert(`Boas vindas ${nome}`);
+*/
+
+
